52ae6b89d28e63403aae9f586f584820dff423d0
Mox90 <moctar.sandalo89@gmail.com>
2026-03-06 15:50:42 +0300

    install cors package

--- enayah-backend/src/app.ts
+++ enayah-backend/src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 import helmet from 'helmet'
+import cors from 'cors'
 import routes from './routes'
 
 const app = express()
 
 //app.use(helmet())
-//app.use(cors())
+/*app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }),
+)*/
 //app.use(express.json())
 
 app.use('/api/v1', routes)
